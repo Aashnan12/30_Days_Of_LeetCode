d0d08e359b23b690616ebade7e4f65d62ce3ee43
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-28 18:50:16 +0530

    Time: 26 ms (21.98%), Space: 21.3 MB (7.81%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,45 @@
+#pragma GCC optimize("Ofast")
+static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
 class Solution {
 public:
-    // We have to calc no. of provinces....
-    void dfs(unordered_map<int,vector<int>> &map,int u, vector<bool> &vis){
+    void bfs(unordered_map<int,vector<int>> &map,vector<bool> &vis,int src){
+        queue<int> q;
+        q.push(src);
+        vis[src] = true;
 
-        vis[u] = true;
-
-        for(int &v : map[u]){
-            if(!vis[v]){
-                dfs(map,v,vis);
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int &v : map[u]){
+                if(!vis[v]){
+                    q.push(v);
+                    vis[v] = true;
+                }
             }
         }
-        return ;
+        return;
     }
-    int findCircleNum(vector<vector<int>>& mat) {
-        int n = mat.size();
+    int findCircleNum(vector<vector<int>>& isConnected) {
         unordered_map<int,vector<int>> map;
+        int n = isConnected.size();
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++) {
-                if(i!=j && mat[i][j] == 1){
+            for(int j=0;j<n;j++){
+                if(i != j && isConnected[i][j] == 1){
                     map[i].push_back(j);
                     map[j].push_back(i);
                 }
             }
         }
-        int provinces = 0;
-        vector<bool> vis(n+1,0);
+        int pro = 0;
+        vector<bool> vis(n+1,false);
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(map,i,vis);
-                provinces++;
+                pro++;
+                bfs(map,vis,i);
             }
         }
-        return provinces;
+        return pro;
     }
 };
