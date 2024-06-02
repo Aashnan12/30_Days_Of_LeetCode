0c96bc7dcad796a5b4da31f85170cf3c6d24a3d4
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-02 18:57:35 +0530

    Time: 0 ms (100%), Space: 7.1 MB (86.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n==0 || n==1)return n;
-        int a = 0;
-        int b = 1;
-        int num = 0;
-        while(n--){
-            num = a + b;
-            a = b;
-            b = num;
+    int dp[46];
+    int solve(int stair,int top){
+        if(stair > top){
+            return 0;
         }
-        return num;
+        if(dp[stair] != -1) {
+            return dp[stair];
+        }
+        if(stair == top){
+            return 1;
+        }
+        int one_step = solve(stair+1,top);
+        int two_step = solve(stair+2,top);
+        return dp[stair] = one_step + two_step;
+    }
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        solve(0,n);
+        for(int i=0;i<=n;i++){
+            cout<<dp[i]<<" ";
+        }
+        return dp[0];
     }
 };
