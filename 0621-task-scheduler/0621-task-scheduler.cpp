a11f891cbc4598074263462097fcac093c2613a4
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-03 13:51:42 +0530

    Time: 49 ms (76.28%), Space: 37.9 MB (92.22%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int hash[26] = {0};
+        int count,maxi=INT_MIN;
+        for(char& ch : tasks){
+            hash[ch - 'A']++;
+            maxi = max(maxi,hash[ch-'A']);
+        }
+        count = (maxi-1)*(n+1);
+        for(int i=0;i<26;i++){
+            if(hash[i] == maxi){
+                count++;
+            }
+        }
+        return max(count,(int)tasks.size());
+    }
+};
