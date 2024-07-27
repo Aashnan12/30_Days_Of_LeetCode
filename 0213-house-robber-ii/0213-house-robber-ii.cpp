602420524b60dbe1ed8d349a1a40093437a78cdc
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-07-27 15:22:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (26.71%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -14,6 +14,7 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
+        if(n==1) return nums[0];
         memset(t,-1,sizeof(t));
         int possibility1 = solve(0,nums,n-1);
         memset(t,-1,sizeof(t));
